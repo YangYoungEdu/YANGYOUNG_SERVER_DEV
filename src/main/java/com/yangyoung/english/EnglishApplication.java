d40a7ee1930efde614156777654bd1762f2e2ee1
dbseend <shyoon840@gmail.com>
2024-07-24 01:40:52 +0900

    set time zone

--- src/main/java/com/yangyoung/english/EnglishApplication.java
+++ src/main/java/com/yangyoung/english/EnglishApplication.java
@@ -1,11 +1,14 @@
 package com.yangyoung.english;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,5 +17,11 @@ public class EnglishApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EnglishApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
