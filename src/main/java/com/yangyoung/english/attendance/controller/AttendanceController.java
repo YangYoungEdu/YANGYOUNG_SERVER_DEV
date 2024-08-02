f0c858e5fdc56aafdb508095eda8650fbeefcc43
dbseend <shyoon840@gmail.com>
2024-08-02 22:50:14 +0900

    .

--- src/main/java/com/yangyoung/english/attendance/controller/AttendanceController.java
+++ src/main/java/com/yangyoung/english/attendance/controller/AttendanceController.java
@@ -22,7 +22,8 @@ public class AttendanceController {
     // 출석 - 학생
     @PostMapping("/{studentId}")
     @Operation(summary = "학생 출석 - client", description = "학생의 출석을 처리합니다.")
-    public ResponseEntity<AttendanceResponse> attend(@PathVariable(value = "studentId") Long studentId) {
+    public ResponseEntity<AttendanceResponse> attend(@PathVariable(value = "studentId") Long studentId,
+                                                     @RequestHeader(value = "Authorization") String token) {
 
         AttendanceResponse response = attendanceService.attend(studentId);
 
